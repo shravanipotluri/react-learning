835b0fc6dee71ce29bf706c34e1f449906d5ed6d
shravani-vallurupalli <83086286+shravani-vallurupalli@users.noreply.github.com>
2025-05-27 14:32:34 -0500

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abc" },
@@ -21,42 +24,42 @@ root.render(heading);
 // The render function is used to render the react element.
 
 //Nested React Elements
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am an h1 tag")
-  )
-);
-const root2 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(parent);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "I am an h1 tag")
+//   )
+// );
+// const root2 = ReactDOM.createRoot(document.getElementById("root"));
+// root2.render(parent);
 
 // If we want to have two children in the parent element, we can do it like this
-const parent1 = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
-const root3 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(parent1);
+// const parent1 = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am an h1 tag"),
+//     React.createElement("h2", {}, "I am an h2 tag"),
+//   ])
+// );
+// const root3 = ReactDOM.createRoot(document.getElementById("root"));
+// root2.render(parent1);
 
 // If we have to create 2 children in the parent element, we can do it like this. But it is becoming complex and unmaintainable.so we use JSX syntax.
 // JSX syntax is a syntax extension for JavaScript that looks similar to HTML.
 // JSX syntax is used to create react elements.
-const parent2 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
-const root4 = ReactDOM.createRoot(document.getElementById("root"));
-root4.render(parent2);
+// const parent2 = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { key: 1234 }, "I am an h1 tag"),
+//     React.createElement("h2", { key: 1235 }, "I am an h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", { key: 1236 }, "I am an h1 tag"),
+//     React.createElement("h2", { key: 1237 }, "I am an h2 tag"),
+//   ]),
+// ]);
+// const root4 = ReactDOM.createRoot(document.getElementById("root"));
+// root4.render(parent2);
